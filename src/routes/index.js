8c0684bab13a6d9f3a38ebb3a82f15d59ed722ab
lensonlovescode <lensonisinparis@gmail.com>
2025-06-27 09:58:56 +0300

    Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+const express = require('express');
+const UserController = reqire('../controllers/UserController');
+
+const router = express.Router();
+
+router.post('/signup', UserController.SignMeUp)
+router.post('/login', UserController.LogMeIn)
+router.post('/logout', UserController.LogMeOut)
+router.post('/forgotpassword', UserController.ForgotPassword)
+
+module.export = router;
